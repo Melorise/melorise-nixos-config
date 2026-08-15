105084b3a5f2ad00d244f488601b750c9443af65
Melorise <0d00@0721.hk>
2026-08-15 19:43:34 +0800

    home: wrap dsh with required node flag

--- home/tippy/development/nodejs.nix
+++ home/tippy/development/nodejs.nix
@@ -9,7 +9,16 @@
     NPM_CONFIG_PREFIX = "$HOME/.npm-global";
   };
 
+  home.file.".local/bin/dsh" = {
+    executable = true;
+    text = ''
+      #!${pkgs.runtimeShell}
+      exec ${pkgs.nodejs_24}/bin/node --expose-internals "$HOME/.npm-global/bin/dsh" "$@"
+    '';
+  };
+
   home.sessionPath = [
+    "$HOME/.local/bin"
     "$HOME/.npm-global/bin"
   ];
 }
